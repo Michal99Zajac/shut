b093fdad2f710c76289b3bf53a48931847018adb
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-15 15:07:01 +0200

    feat(app): add graphql endpoint

---
+++ app/graphql/route.ts
@@ -0,0 +1,9 @@
+import { handler } from '@/graphql'
+
+export async function GET(request: Request) {
+  return handler(request)
+}
+
+export async function POST(request: Request) {
+  return handler(request)
+}
