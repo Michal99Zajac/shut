47e5e4164a71f40d26a46ea369366b979dee42d9
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-15 21:29:09 +0200

    feat(db): add description to the Bookmark model

--- prisma/migrations/20230815192827_add_bookmark_model/migration.sql
+++ prisma/migrations/20230815192827_add_bookmark_model/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "Bookmark" (
     "id" TEXT NOT NULL,
     "url" TEXT NOT NULL,
     "frindlyName" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
