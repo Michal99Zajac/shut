895625d21162af95acb91189a4ce63ed6317c0c0
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-15 21:03:43 +0200

    feat(db): add basic bookmark model

---
+++ prisma/migrations/20230815170843_add_basic_bookmark_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Bookmark" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "frindlyName" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Bookmark_pkey" PRIMARY KEY ("id")
+);
