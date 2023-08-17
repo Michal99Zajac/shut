a6a6f012e4847811b12e5c3abe67ec4037d74344
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-17 22:38:50 +0200

    feat(graphql): add SignedUpResponse model

---
+++ server/auth/graphql/models/SignedUpResponse/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Model for the response of the signed up mutation
+ */
+export class SignedUpResponse {
+  /**
+   * Id of the created user
+   */
+  id!: string
+}
+
+export default SignedUpResponse
