80cd5356a1f4a9cfe16dbceb86d2178fd5e45ebb
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-17 22:45:38 +0200

    feat(graphql): install pothos plugins

--- server/graphql/builder.ts
+++ server/graphql/builder.ts
@@ -1,9 +1,41 @@
 import SchemaBuilder from '@pothos/core'
+import ScopeAuthPlugin from '@pothos/plugin-scope-auth'
+import type PrismaTypes from '@pothos/plugin-prisma/generated'
+import PrismaPlugin from '@pothos/plugin-prisma'
+import ValidationPlugin from '@pothos/plugin-validation'
 
-const builder = new SchemaBuilder({})
+import { prisma } from '#/db'
+
+import { Context, AuthContext } from './context'
+
+interface SchemaTypes {
+  Context: Context
+  AuthScopes: {
+    logged: boolean
+  }
+  AuthContexts: {
+    loggedIn: AuthContext
+  }
+  PrismaTypes: PrismaTypes
+}
+
+const builder = new SchemaBuilder<SchemaTypes>({
+  plugins: [ScopeAuthPlugin, PrismaPlugin, ValidationPlugin],
+  authScopes: (context) => ({
+    logged: !!context.user,
+  }),
+  prisma: {
+    client: prisma,
+  },
+  validationOptions: {
+    validationError: (error, args, context, info) => {
+      return error
+    },
+  },
+})
 
 // Init builder query and mutation types
-// builder.mutationType({})
+builder.mutationType({})
 builder.queryType({})
 
 export default builder
