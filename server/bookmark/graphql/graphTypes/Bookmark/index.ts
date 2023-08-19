5a0e74a8b312c513e9b0ed99a1d18eb740ee4f91
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-19 21:23:50 +0200

    feat(graphql): add Bookmark graph type

---
+++ server/bookmark/graphql/graphTypes/Bookmark/index.ts
@@ -0,0 +1,13 @@
+import builder from '#/graphql/builder'
+
+builder.prismaObject('Bookmark', {
+  name: 'Bookmark',
+  description: 'A bookmark',
+  fields: (t) => ({
+    id: t.exposeID('id'),
+    url: t.exposeString('url'),
+    friendlyName: t.exposeString('friendlyName'),
+    description: t.exposeString('description', { nullable: true }),
+    user: t.relation('user'),
+  }),
+})
