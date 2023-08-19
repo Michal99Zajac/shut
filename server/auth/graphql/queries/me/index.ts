90b057699266edfb5982bed74edb1ca8cf913d35
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-19 19:47:50 +0200

    feat(graphql): add description to the me query

--- server/auth/graphql/queries/me/index.ts
+++ server/auth/graphql/queries/me/index.ts
@@ -5,6 +5,7 @@ import { resolveMe } from '../../resolvers/resolveMe'
 builder.queryField('me', (t) =>
   t.withAuth({ logged: true }).prismaField({
     type: 'User',
+    description: 'Get the current user',
     resolve: resolveMe,
   }),
 )
