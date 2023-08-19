ecc3b44955286c5e95c43edca342792612dd36b6
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-19 17:14:45 +0200

    refactor(CookieService): add comments

--- server/common/service/CookieService/index.ts
+++ server/common/service/CookieService/index.ts
@@ -3,13 +3,24 @@ import { cookies } from 'next/headers'
 
 import { config } from '#/config'
 
+/**
+ * Service class for managing cookies.
+ */
 export class CookieService {
+  /**
+   * Instance of the Next.js cookie management.
+   */
   protected cookie: ReadonlyRequestCookies
 
   constructor() {
     this.cookie = cookies()
   }
 
+  /**
+   * Sets the access token in a cookie with security settings and expiration.
+   *
+   * @param token JWT access token
+   */
   access(token: string) {
     const expires = new Date(Date.now() + config.jwt.access.options.expiresIn * 1000) // 7 days in milliseconds
 
@@ -20,6 +31,9 @@ export class CookieService {
     })
   }
 
+  /**
+   * Removes the access token from cookies.
+   */
   removeAccess() {
     this.cookie.delete('accessToken')
   }
