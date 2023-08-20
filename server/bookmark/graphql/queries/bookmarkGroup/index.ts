e2591a7a3b3d34efbc7bf6fb9e2772a657c133cd
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-20 08:06:55 +0200

    fix(graphql): make bookmarkGroup query

--- server/bookmark/graphql/queries/bookmarkGroup/index.ts
+++ server/bookmark/graphql/queries/bookmarkGroup/index.ts
@@ -2,7 +2,7 @@ import builder from '#/graphql/builder'
 
 import { resolveBookmarkGroup } from '../../resolvers/resolveBookmarkGroup'
 
-builder.mutationField('bookmarkGroup', (t) =>
+builder.queryField('bookmarkGroup', (t) =>
   t.withAuth({ logged: true }).prismaField({
     type: 'BookmarkGroup',
     args: {
