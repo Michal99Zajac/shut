00b2bdb94a79ccf239eaff5f6deb5364b93b106b
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-20 22:37:51 +0200

    feat(bookmark): add resolveUpdateBookmarkGroup resolver

---
+++ server/bookmark/graphql/resolvers/resolveUpdateBookmarkGroup/index.ts
@@ -0,0 +1,55 @@
+import { Prisma } from '@prisma/client'
+import { DefaultArgs } from '@prisma/client/runtime/library'
+
+import { Parent } from '#/common/types/Parent'
+import { Query } from '#/common/types/Query'
+import { InputShape } from '#/common/types/InputShape'
+import { AuthContext } from '#/graphql/context'
+
+import { UpdateBookmarkGroupInput } from '../../inputs/UpdateBookmarkGroupInput'
+
+interface UpdateBookmarkGroupQuery extends Query {
+  include?: Prisma.BookmarkGroupInclude<DefaultArgs>
+  select?: Prisma.BookmarkGroupSelect<DefaultArgs>
+}
+
+interface Args {
+  id: string | number
+  input: InputShape<typeof UpdateBookmarkGroupInput>
+}
+
+/**
+ * GraphQL resolver for updaing a bookmark group.
+ *
+ * @param query UpdateBookmarkGroupQuery
+ * @param _ Parent
+ * @param args Args
+ * @param context AuthContext
+ * @returns Updated bookmark group
+ */
+export const resolveUpdateBookmarkGroup = async (
+  query: UpdateBookmarkGroupQuery,
+  _: Parent,
+  args: Args,
+  context: AuthContext,
+) => {
+  const { input, id } = args
+
+  const updatedBookmarkGroup = context.prisma.bookmarkGroup.update({
+    ...query,
+    where: {
+      id: id.toString(),
+      user: {
+        id: context.user.id,
+      },
+    },
+    data: {
+      description: input.description || undefined,
+      name: input.name || undefined,
+    },
+  })
+
+  return updatedBookmarkGroup
+}
+
+export default resolveUpdateBookmarkGroup
