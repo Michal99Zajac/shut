d7547a5c0f34c1a3c1dd806337f370a37344f183
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-23 20:05:03 +0200

    feat(layout): add basic AuthLayout

--- client/layout/AuthLayout/index.tsx
+++ client/layout/AuthLayout/index.tsx
@@ -5,8 +5,14 @@ export interface AuthLayoutProps {
 export const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <main className="flex flex-row w-full h-full">
-      <section className="bg-primary w-1/2 p-8"></section>
-      <section className="w-1/2 p-8">{children}</section>
+      <section className="bg-primary w-1/2 p-8">
+        <h1 className="font-koulen text-8xl text-white">
+          Save it!
+          <br />
+          Keep it!
+        </h1>
+      </section>
+      <section className="w-1/2 p-8 flex flex-col justify-center items-center">{children}</section>
     </main>
   )
 }
