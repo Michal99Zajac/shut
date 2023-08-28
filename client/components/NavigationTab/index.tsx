2882398f595409c4e4814e5bb60837e256c3be4c
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-28 22:14:40 +0200

    feat(components): add NavigationTab component

---
+++ client/components/NavigationTab/index.tsx
@@ -0,0 +1,27 @@
+import { ActiveLink } from '@/components/ActiveLink'
+
+interface NavigationTabProps {
+  /**
+   * The URL the navigation tab points to.
+   */
+  href: string
+}
+
+/**
+ * A navigation tab component that uses the ActiveLink component
+ * to apply specific styles based on the active state.
+ * This component is placed inside header.
+ */
+export function NavigationTab({ href, children }: React.PropsWithChildren<NavigationTabProps>) {
+  return (
+    <ActiveLink
+      href={href}
+      className="border-b-2 font-koulen px-2 hover:bg-area-hover hover:bg-opacity-5"
+      activeClassName="border-primary"
+    >
+      {children}
+    </ActiveLink>
+  )
+}
+
+export default NavigationTab
