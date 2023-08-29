eb19e5e3d222a3bbd656e484b1d90f27bb7b0882
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-30 00:17:37 +0200

    feat(client): make root page simpler

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,24 +1,9 @@
-'use client'
-
-import { gql, useSuspenseQuery } from '@apollo/client'
-
-const meQuery = gql`
-  query Me {
-    me {
-      id
-      email
-    }
-  }
-`
-
 export function RootPage() {
-  const { data } = useSuspenseQuery(meQuery)
-
   return (
-    <div>
-      <div>Root Page</div>
-      <div>{JSON.stringify(data)}</div>
-    </div>
+    <>
+      <h1 className="font-koulen text-4xl">Dashboard</h1>
+      <p className="text-gray-500">Welcome to your dashboard!</p>
+    </>
   )
 }
 
