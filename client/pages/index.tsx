3e74e95f13fc8c6169cbd855c0688631dfe57f97
Michal99Zajac <michal.99.zajac@gmail.com>
2023-08-30 22:58:41 +0200

    feat(components): init BookmarkGroupTree component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,4 @@
-'use client'
-
-import { AiOutlineFolder, AiFillFolder } from 'react-icons/ai'
-import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io'
-
-import { TreeItem, TreeView } from '@/components/Tree'
+import BookmarkGroupTree from '@/bookmarks/components/BookmarkGroupTree'
 
 export function RootPage() {
   return (
@@ -11,24 +6,7 @@ export function RootPage() {
       <h1 className="font-koulen text-4xl">Dashboard</h1>
       <p className="text-gray-500">Welcome to your dashboard!</p>
       <div>
-        <TreeView
-          aria-label="gmail"
-          defaultExpanded={['3']}
-          defaultCollapseIcon={<IoMdArrowDropdown />}
-          defaultExpandIcon={<IoMdArrowDropup />}
-          defaultEndIcon={<div style={{ width: 24 }} />}
-          sx={{ height: 264, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
-        >
-          <TreeItem nodeId="1" labelText="All Mail" labelIcon={AiFillFolder} labelInfo="90" />
-          <TreeItem nodeId="2" labelText="Trash" labelIcon={AiFillFolder} labelInfo="333" />
-          <TreeItem nodeId="3" labelText="Categories" labelIcon={AiOutlineFolder} labelInfo="30">
-            <TreeItem nodeId="5" labelText="Social" labelIcon={AiFillFolder} labelInfo="90" />
-            <TreeItem nodeId="6" labelText="Updates" labelIcon={AiFillFolder} labelInfo="2,294" />
-            <TreeItem nodeId="7" labelText="Forums" labelIcon={AiFillFolder} labelInfo="3,566" />
-            <TreeItem nodeId="8" labelText="Promotions" labelIcon={AiFillFolder} labelInfo="733" />
-          </TreeItem>
-          <TreeItem nodeId="4" labelText="History" labelIcon={AiFillFolder} labelInfo="2" />
-        </TreeView>
+        <BookmarkGroupTree />
       </div>
     </>
   )
