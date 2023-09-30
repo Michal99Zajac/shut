9ce5bf7092f0d8b8216cc59d7c8fef007422b739
Michal99Zajac <michal.99.zajac@gmail.com>
2023-09-30 14:56:54 +0200

    feat(graphql): add resolveBookmarkGroupPath resolver

---
+++ server/bookmark/graphql/resolvers/resolveBookmarkGroupPath/index.ts
@@ -0,0 +1,40 @@
+import { BookmarkGroup } from '@prisma/client'
+
+import { Context } from '#/graphql/context'
+import { generateBookmarkGroupInclude } from '#/bookmark/utils/generateBookmarkGroupInclude'
+import { getBookmarkGroupPath } from '#/bookmark/utils/getBookmarkGroupPath'
+
+type Parent = BookmarkGroup
+
+interface Args {}
+
+/**
+ * GraphQL resolver for retrieving the path of a bookmark group.
+ *
+ * @param bookmarkGroup Parent
+ * @param _ Args
+ * @param context Context
+ * @returns Path of the given bookmark group
+ */
+export const resolveBookmarkGroupPath = async (
+  bookmarkGroup: Parent,
+  _: Args,
+  context: Context,
+) => {
+  if (!bookmarkGroup.parentId) {
+    return parent.name
+  }
+
+  // Get the nested bookmark group
+  const nestedBookmarkGroup = await context.prisma.bookmarkGroup.findFirstOrThrow({
+    where: {
+      id: bookmarkGroup.id,
+    },
+    include: generateBookmarkGroupInclude(bookmarkGroup.depth),
+  })
+
+  // Generate the path
+  return getBookmarkGroupPath(nestedBookmarkGroup).join(' / ')
+}
+
+export default resolveBookmarkGroupPath
