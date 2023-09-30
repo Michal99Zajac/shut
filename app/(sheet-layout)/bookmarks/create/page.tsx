1b1facf2969b27275289f7a53ad8cf5e94946478
Michal99Zajac <michal.99.zajac@gmail.com>
2023-09-30 18:38:41 +0200

    fix(CreateBookmarkPage): remove isModal prop

--- app/(sheet-layout)/bookmarks/create/page.tsx
+++ app/(sheet-layout)/bookmarks/create/page.tsx
@@ -1,4 +1,4 @@
 import { CreateBookmarkPage, CreateBookmarkPageProps } from '@/pages/bookmarks/create'
 
 // eslint-disable-next-line react/display-name, import/no-anonymous-default-export
-export default (props: CreateBookmarkPageProps) => <CreateBookmarkPage {...props} isModal />
+export default (props: CreateBookmarkPageProps) => <CreateBookmarkPage {...props} />
