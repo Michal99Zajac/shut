a0fc805fec985642dd4240d6eb385f8d3d9cc339
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-01 22:39:50 +0200

    feat(api): handle email confirmation

---
+++ app/api/auth/confirm-email/route.ts
@@ -0,0 +1,1 @@
+export { GET } from '#/auth/api/confirm-email'
---
+++ server/auth/api/confirm-email/index.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { config } from '#/config'
+import { prisma } from '#/db'
+import AuthenticationService from '#/auth/services/AuthenticationService'
+
+export const GET = async (req: NextRequest) => {
+  const token = req.nextUrl.searchParams.get('token')
+
+  if (!token) {
+    return new Response('Token not found', { status: 400 })
+  }
+
+  // start auth service
+  const authenticator = new AuthenticationService(prisma)
+
+  // decode token
+  try {
+    const user = await authenticator.decodeShort(token, 'VERIFY')
+
+    // change user status
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { confirmed: true },
+    })
+
+    // redirect to login page
+    return NextResponse.redirect(`${config.server.url}/auth/signin`)
+  } catch (error) {
+    return new Response('Token or user is invalid', { status: 400 })
+  }
+}
