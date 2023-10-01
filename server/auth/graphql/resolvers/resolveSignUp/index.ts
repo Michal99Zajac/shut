24212f1afe3eee4d1ddad0f6be84a74ad7d0e0c2
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-01 22:39:03 +0200

    feat(signup): send confirmation email

--- server/auth/graphql/resolvers/resolveSignUp/index.ts
+++ server/auth/graphql/resolvers/resolveSignUp/index.ts
@@ -1,8 +1,12 @@
 import { GraphQLError } from 'graphql'
+import { SendTemplatedEmailCommand } from '@aws-sdk/client-ses'
 
+import { ses } from '#/aws/ses'
 import { Context } from '#/graphql/context'
 import InputShape from '#/common/types/InputShape'
 import Parent from '#/common/types/Parent'
+import { config } from '#/config'
+import AuthenticationService from '#/auth/services/AuthenticationService'
 
 import { SignUpInput } from '../../inputs/SignUpInput'
 
@@ -32,6 +36,29 @@ export const resolveSignUp = async (_: Parent, args: Args, context: Context) => 
 
   // sign up user
   user = await context.prisma.user.signUp(email, password)
+
+  try {
+    const authenticator = new AuthenticationService(context.prisma)
+
+    // generate token
+    const token = await authenticator.encodeShort(user, 'VERIFY')
+
+    const command = new SendTemplatedEmailCommand({
+      Source: `SHUT <${config.aws.sender}>`,
+      Destination: {
+        ToAddresses: [config.aws.sender], // FIXME: change to correct addesss
+      },
+      Template: 'shut-confirm-email',
+      TemplateData: JSON.stringify({
+        email: user.email,
+        confirmLink: `${config.server.url}/api/auth/confirm-email?token=${token}`,
+      }),
+    })
+
+    ses.send(command)
+  } catch (error) {
+    throw new GraphQLError('Email send went wrong')
+  }
 
   // Return user id
   return user.id
