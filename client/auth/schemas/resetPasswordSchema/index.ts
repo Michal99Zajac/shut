89602fa7e3768c4ba73270ef70127e6055254a9f
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-02 11:48:44 +0200

    feat(schemas): remove resetPasswordSchema

--- client/auth/schemas/resetPasswordSchema/index.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const resetPasswordSchema = z.object({
-  password: z.string(),
-  repeatedPassword: z.string(),
-})
-
-export type ResetPasswordSchema = z.infer<typeof resetPasswordSchema>
