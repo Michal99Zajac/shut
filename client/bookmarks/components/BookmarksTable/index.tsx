84ab6e975389fed18d9854b740d59c287ded7d0c
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-02 23:05:30 +0200

    feat(BookmarksTable): make component more mobile like

--- client/bookmarks/components/BookmarksTable/index.tsx
+++ client/bookmarks/components/BookmarksTable/index.tsx
@@ -7,12 +7,14 @@ import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 import A from '@mui/material/Link'
 import Link from 'next/link'
-import { BiEdit } from 'react-icons/bi'
+import { BiEdit, BiMenu } from 'react-icons/bi'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
+import { useState } from 'react'
 
 import { GQL_BookmarksQuery } from '@/graphql/generated'
 import DeleteBookmarkIconButton from '@/bookmarks/components/DeleteBookmarkIconButton'
+import BookmarkMenu, { AnchorEl } from '@/bookmarks/components/BookmarkMenu'
 
 import { BookmarksTableEmpty } from './components/BookmarksTableEmpty'
 
@@ -21,72 +23,91 @@ interface BookmarksTableProps {
 }
 
 export function BookmarksTable({ bookmarks }: BookmarksTableProps) {
+  const [anchorEl, setAnchorEl] = useState<AnchorEl | null>(null)
+
   if (bookmarks.edges.length === 0) return <BookmarksTableEmpty />
 
   return (
-    <TableContainer
-      component={Paper}
-      sx={{
-        background: '#f8fafc',
-        thead: {
-          th: {
-            borderBottom: '2px solid #f8fafc',
+    <>
+      <TableContainer
+        component={Paper}
+        sx={{
+          background: '#f8fafc',
+          thead: {
+            th: {
+              borderBottom: '2px solid #f8fafc',
+            },
           },
-        },
-        tbody: {
-          td: {
-            borderBottom: 'none',
+          tbody: {
+            td: {
+              borderBottom: 'none',
+            },
           },
-        },
-      }}
-      className="!border-2 !border-gray-50 !shadow-none"
-    >
-      <Table aria-label="bookmarks-table">
-        <TableHead>
-          <TableRow>
-            <TableCell component="th">
-              <span className="font-semibold text-gray-400">Name</span>
-            </TableCell>
-            <TableCell component="th">
-              <span className="font-semibold text-gray-400">URL</span>
-            </TableCell>
-            <TableCell component="th">
-              <span className="font-semibold text-gray-400">Group</span>
-            </TableCell>
-            <TableCell component="th" />
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {bookmarks.edges.map(({ node }) => (
-            <TableRow key={node.id}>
-              <TableCell>{node.friendlyName}</TableCell>
-              <TableCell>
-                <A href={node.url} target="_blank">
-                  {node.url}
-                </A>
+        }}
+        className="!border-2 !border-gray-50 !shadow-none"
+      >
+        <Table aria-label="bookmarks-table">
+          <TableHead>
+            <TableRow>
+              <TableCell component="th">
+                <span className="font-semibold text-gray-400">Name</span>
               </TableCell>
-              <TableCell>
-                <span className="rounded-full px-2 bg-primary-200 leading-6 text-primary inline-block">
-                  {node.bookmarkGroup.path}
-                </span>
+              <TableCell component="th">
+                <span className="font-semibold text-gray-400">URL</span>
               </TableCell>
-              <TableCell align="right">
-                <div className="inline-flex gap-2">
-                  <Tooltip title="Update bookmark">
-                    <Link href={`/bookmarks/bookmark/${node.id}`}>
-                      <IconButton size="small" className="!rounded">
-                        <BiEdit />
-                      </IconButton>
-                    </Link>
-                  </Tooltip>
-                  <DeleteBookmarkIconButton bookmarkId={node.id} />
-                </div>
+              <TableCell component="th" className="!hidden md:!block">
+                <span className="font-semibold text-gray-400">Group</span>
               </TableCell>
+              <TableCell component="th" />
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {bookmarks.edges.map(({ node }) => (
+              <TableRow key={node.id}>
+                <TableCell>{node.friendlyName}</TableCell>
+                <TableCell>
+                  <A href={node.url} target="_blank">
+                    {node.url}
+                  </A>
+                </TableCell>
+                <TableCell className="!hidden md:!block">
+                  <span className="rounded-full px-2 bg-primary-200 leading-6 text-primary inline-block">
+                    {node.bookmarkGroup.path}
+                  </span>
+                </TableCell>
+                <TableCell align="right">
+                  <div className="gap-2 hidden md:inline-flex">
+                    <Tooltip title="Update bookmark">
+                      <Link href={`/bookmarks/bookmark/${node.id}`}>
+                        <IconButton size="small" className="!rounded">
+                          <BiEdit />
+                        </IconButton>
+                      </Link>
+                    </Tooltip>
+                    <DeleteBookmarkIconButton bookmarkId={node.id} />
+                  </div>
+                  <Tooltip title="Bookmark menu">
+                    <IconButton
+                      onClick={(event) =>
+                        setAnchorEl({
+                          target: event.currentTarget,
+                          id: node.id,
+                        })
+                      }
+                      size="small"
+                      className="!rounded md:!hidden"
+                    >
+                      <BiMenu />
+                    </IconButton>
+                  </Tooltip>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <BookmarkMenu anchorEl={anchorEl} onClose={() => setAnchorEl(null)} />
+    </>
   )
 }
 
