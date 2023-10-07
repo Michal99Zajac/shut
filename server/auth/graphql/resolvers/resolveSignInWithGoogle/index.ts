6b0df487a3f05b35b620642a54014c329d3e3033
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-07 08:25:51 +0200

    feat(resolvers): add resolveSignInWithGoogle

---
+++ server/auth/graphql/resolvers/resolveSignInWithGoogle/index.ts
@@ -0,0 +1,146 @@
+import { OAuth2Client } from 'google-auth-library'
+import { GraphQLError } from 'graphql'
+
+import { config } from '#/config'
+import { CryptoService } from '#/secure/services/CryptoService'
+import { googleTokenPayloadSchema } from '#/auth/schemas/GoogleTokenPayload'
+import { googleAccessDataSchema } from '#/auth/schemas/GoogleAccessData'
+import Parent from '#/common/types/Parent'
+import Query from '#/common/types/Query'
+import { Context } from '#/graphql/context'
+import InputShape from '#/common/types/InputShape'
+
+import { OAuthCodeInput } from '../../inputs/OAuthCodeInput'
+
+interface Args {
+  input: InputShape<typeof OAuthCodeInput>
+}
+
+/**
+ * Resolver for the signInWithGoogle mutation. Sign in with Google.
+ *
+ * @param query Query
+ * @param parent Parent
+ * @param args Args
+ * @param context Context
+ * @returns user id if successful otherwise throws an error
+ */
+export const resolveSignInWithGoogle = async (
+  query: Query,
+  parent: Parent,
+  args: Args,
+  context: Context,
+) => {
+  // prepare the crypto service
+  const cryptoService = new CryptoService()
+
+  const oauth = new OAuth2Client({
+    clientId: config.oauth.google.clientId,
+    clientSecret: config.oauth.google.clientSecret,
+    redirectUri: config.oauth.google.redirectUri,
+  })
+
+  try {
+    // decode the code
+    const { tokens } = await oauth.getToken(args.input.code)
+
+    const { id_token: idToken, ...accessData } = tokens
+
+    if (!idToken) throw new Error('Getting id token failed')
+
+    // parse the access data
+    const parsedAccessData = googleAccessDataSchema.parse(accessData)
+
+    // verify the id token and get the ticket
+    const ticket = await oauth.verifyIdToken({
+      idToken,
+    })
+
+    // get the payload from the ticket
+    const tokenPayload = ticket.getPayload()
+
+    if (!tokenPayload) throw new Error('Get payload failed')
+
+    // parse the token payload
+    const parsedTokenPayload = googleTokenPayloadSchema.parse(tokenPayload)
+
+    // encrypt the access data
+    const { content, iv } = cryptoService.encrypt(parsedAccessData)
+
+    // mark the user as confirmed because they have signed in with Google
+
+    return context.prisma.user.upsert({
+      ...query,
+      where: {
+        email: parsedTokenPayload.email,
+      },
+      create: {
+        email: parsedTokenPayload.email,
+        confirmed: true,
+        googleUser: {
+          connectOrCreate: {
+            where: {
+              email: parsedTokenPayload.email,
+            },
+            create: {
+              email: parsedTokenPayload.email,
+              locale: parsedTokenPayload.locale,
+              picture: parsedTokenPayload.picture,
+              familyName: parsedTokenPayload.family_name,
+              givenName: parsedTokenPayload.given_name,
+              name: parsedTokenPayload.name,
+              googleToken: {
+                create: {
+                  data: content,
+                  iv,
+                },
+              },
+            },
+          },
+        },
+      },
+      update: {
+        confirmed: true,
+        googleUser: {
+          upsert: {
+            where: {
+              email: parsedTokenPayload.email,
+            },
+            create: {
+              email: parsedTokenPayload.email,
+              locale: parsedTokenPayload.locale,
+              picture: parsedTokenPayload.picture,
+              familyName: parsedTokenPayload.family_name,
+              givenName: parsedTokenPayload.given_name,
+              name: parsedTokenPayload.name,
+              googleToken: {
+                create: {
+                  data: content,
+                  iv,
+                },
+              },
+            },
+            update: {
+              email: parsedTokenPayload.email,
+              locale: parsedTokenPayload.locale,
+              picture: parsedTokenPayload.picture,
+              familyName: parsedTokenPayload.family_name,
+              givenName: parsedTokenPayload.given_name,
+              name: parsedTokenPayload.name,
+              googleToken: {
+                update: {
+                  data: content,
+                  iv,
+                },
+              },
+            },
+          },
+        },
+      },
+    })
+  } catch (error) {
+    throw new GraphQLError('Google OAuth failed')
+  }
+}
+
+export default resolveSignInWithGoogle
