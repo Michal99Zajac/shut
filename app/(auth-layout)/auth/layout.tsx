a21b94b0dfe3406cfd0cac26cb95c65bc5afe384
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-07 17:38:16 +0200

    feat(app): update pages metadata titles

--- app/(auth-layout)/auth/layout.tsx
+++ app/(auth-layout)/auth/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Shut - Authentication',
+  title: 'Shut | Authentication',
 }
 
 export { default } from '@/layouts/AuthLayout'
--- app/(sheet-layout)/bookmarks/create/page.tsx
+++ app/(sheet-layout)/bookmarks/create/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next'
+
 import { CreateBookmarkPage, CreateBookmarkPageProps } from '@/pages/bookmarks/create'
+
+export const metadata: Metadata = {
+  title: 'Shut | Create Bookmark',
+}
 
 // eslint-disable-next-line react/display-name, import/no-anonymous-default-export
 export default (props: CreateBookmarkPageProps) => <CreateBookmarkPage {...props} />
