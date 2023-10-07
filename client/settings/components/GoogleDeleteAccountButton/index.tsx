0e20ce9dfc8150c81335bc61645e5b5d7845e66d
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-07 17:40:07 +0200

    feat(components): add GoogleDeleteAccountButton component

---
+++ client/settings/components/GoogleDeleteAccountButton/index.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { useState } from 'react'
+import Alert from '@mui/material/Alert'
+import Button from '@mui/material/Button'
+import Snackbar from '@mui/material/Snackbar'
+import { useGoogleLogin } from '@react-oauth/google'
+import { FcGoogle } from 'react-icons/fc'
+import { useRouter } from 'next/navigation'
+
+import { useDeleteAccountWithGoogleMutation } from '@/graphql/generated'
+
+export function GoogleDeleteAccountButton() {
+  const router = useRouter()
+  const [isError, setIsError] = useState(false)
+  const [deleteAccount, { loading }] = useDeleteAccountWithGoogleMutation({
+    onError: () => {
+      setIsError(true)
+    },
+    onCompleted: () => {
+      router.push('/auth/signin')
+    },
+  })
+  const login = useGoogleLogin({
+    flow: 'auth-code',
+    onSuccess: async (codeResponse) => {
+      await deleteAccount({
+        variables: {
+          input: {
+            code: codeResponse.code,
+          },
+        },
+      })
+    },
+    onError: (error) => {
+      console.error(error)
+      setIsError(true)
+    },
+  })
+
+  return (
+    <>
+      <Button
+        startIcon={<FcGoogle />}
+        disabled={loading}
+        className="!font-roboto !text-black !border-blue-100 hover:!bg-blue-50"
+        fullWidth
+        variant="outlined"
+        size="large"
+        onClick={login}
+      >
+        Confirm deletion thanks to Google
+      </Button>
+      <Snackbar open={isError} autoHideDuration={6000} onClose={() => setIsError(false)}>
+        <Alert onClose={() => setIsError(false)} severity="error" sx={{ width: '100%' }}>
+          Google confirmation has failed
+        </Alert>
+      </Snackbar>
+    </>
+  )
+}
+
+export default GoogleDeleteAccountButton
