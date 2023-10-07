660ff06f2b3a854092f837fd8ce2e02a75fe87cd
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-07 08:13:51 +0200

    feat(resolveChangePassword): check if password exists

--- server/auth/graphql/resolvers/resolveChangePassword/index.ts
+++ server/auth/graphql/resolvers/resolveChangePassword/index.ts
@@ -23,6 +23,8 @@ export const resolveChangePassword = async (parent: Parent, args: Args, context:
   const { input } = args
   const { user } = context
 
+  if (!context.user.password) throw new GraphQLError('User does not have a password')
+
   // check if old password is correct
   const isCorrect = bcrypt.compare(input.oldPassword, context.user.password)
   if (!isCorrect) throw new GraphQLError('Old password is incorrect')
