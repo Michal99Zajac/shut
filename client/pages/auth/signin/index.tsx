764cde77229289a3f0ab9300900ce4b52f6bc8c0
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-07 09:53:35 +0200

    feat(signin): add error snackbar handler

--- client/pages/auth/signin/index.tsx
+++ client/pages/auth/signin/index.tsx
@@ -1,6 +1,9 @@
 'use client'
 
-import { Button, TextField } from '@mui/material'
+import Snackbar from '@mui/material/Snackbar'
+import Alert from '@mui/material/Alert'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 import Link from 'next/link'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -12,7 +15,7 @@ import { GoogleLoginButton } from '@/auth/components/GoogleLoginButton'
 
 export const SignInPage = () => {
   const router = useRouter()
-  const [signIn, { loading }] = useSignInMutation()
+  const [signIn, { loading, error, reset }] = useSignInMutation()
   const { register, handleSubmit } = useForm<SignInInputSchema>({
     resolver: zodResolver(signInInputSchema),
   })
@@ -29,55 +32,73 @@ export const SignInPage = () => {
   })
 
   return (
-    <form onSubmit={onSubmit} className="w-[400px] max-w-full">
-      <h1 className="font-koulen text-5xl mb-2">Sign In</h1>
-      <h2 className="mb-8">Welcome back! Sign in to your account!</h2>
-      <TextField
-        {...register('email')}
-        fullWidth
-        label="Email"
-        margin="normal"
-        size="medium"
-        type="email"
-        variant="outlined"
-        required
-        disabled={loading}
-      />
-      <TextField
-        {...register('password')}
-        fullWidth
-        label="Password"
-        type="password"
-        margin="normal"
-        size="medium"
-        variant="outlined"
-        required
-        disabled={loading}
-      />
-      <div className="text-right">
-        <Link href="/auth/forgot-password" className="text-sm text-gray-500 my-2 underline">
-          Forgot password?
-        </Link>
-      </div>
-      <Button
-        disabled={loading}
-        fullWidth
-        type="submit"
-        sx={{ mt: '16px' }}
-        size="large"
-        variant="contained"
+    <>
+      <form onSubmit={onSubmit} className="w-[400px] max-w-full">
+        <h1 className="font-koulen text-5xl mb-2">Sign In</h1>
+        <h2 className="mb-8">Welcome back! Sign in to your account!</h2>
+        <TextField
+          {...register('email')}
+          fullWidth
+          label="Email"
+          margin="normal"
+          size="medium"
+          type="email"
+          variant="outlined"
+          required
+          disabled={loading}
+        />
+        <TextField
+          {...register('password')}
+          fullWidth
+          label="Password"
+          type="password"
+          margin="normal"
+          size="medium"
+          variant="outlined"
+          required
+          disabled={loading}
+        />
+        <div className="text-right">
+          <Link href="/auth/forgot-password" className="text-sm text-gray-500 my-2 underline">
+            Forgot password?
+          </Link>
+        </div>
+        <Button
+          disabled={loading}
+          fullWidth
+          type="submit"
+          sx={{ mt: '16px' }}
+          size="large"
+          variant="contained"
+        >
+          Sign In
+        </Button>
+        <p className="text-right text-sm my-2">
+          Are you new here?{' '}
+          <Link href="/auth/signup" className="text-primary underline">
+            Sign Up!
+          </Link>
+        </p>
+        <hr className="my-4" />
+        <GoogleLoginButton />
+      </form>
+      <Snackbar
+        anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
+        open={!!error}
+        autoHideDuration={6000}
+        onClose={reset}
       >
-        Sign In
-      </Button>
-      <p className="text-right text-sm my-2">
-        Are you new here?{' '}
-        <Link href="/auth/signup" className="text-primary underline">
-          Sign Up!
-        </Link>
-      </p>
-      <hr className="my-4" />
-      <GoogleLoginButton />
-    </form>
+        <Alert
+          title="Signing Up"
+          variant="filled"
+          onClose={reset}
+          severity="error"
+          sx={{ width: '100%' }}
+        >
+          Signing Up Has Failed
+        </Alert>
+      </Snackbar>
+    </>
   )
 }
 
