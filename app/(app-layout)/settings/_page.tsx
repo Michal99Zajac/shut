21fa5c3ed25bcaca485cf0177420fa579da52624
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-11 00:56:08 +0200

    feat(settings): add basic settings layout

--- app/(app-layout)/settings/_page.tsx
+++ app/(app-layout)/settings/_page.tsx
@@ -1,7 +1,72 @@
+import Breadcrumbs from '@mui/material/Breadcrumbs'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Link from 'next/link'
+
 import GoogleDeleteAccountButton from '@/settings/components/GoogleDeleteAccountButton'
 
 const Page: Client.Page = () => {
-  return <GoogleDeleteAccountButton />
+  return (
+    <>
+      <h1 className="font-koulen text-4xl">Settings</h1>
+      <Breadcrumbs aria-label="breadcrumb" className="!mb-4">
+        <Link href="/" className="font-koulen hover:underline">
+          SHUT
+        </Link>
+        <p className="font-koulen">Settings</p>
+      </Breadcrumbs>
+      <div className="flex justify-center mb-8">
+        <div className="w-[620px]">
+          <h2 className="font-koulen text-3xl border-b-2 border-gray-200 border-solid mb-6">
+            Profile
+          </h2>
+          <form>
+            <TextField label="Email" className="!mb-4" fullWidth placeholder="email@example.com" />
+            <div className="text-right">
+              <Button size="large" disabled variant="contained">
+                Update
+              </Button>
+            </div>
+          </form>
+          <h2 className="font-koulen text-3xl border-b-2 border-gray-200 border-solid mb-4">
+            Secure
+          </h2>
+          <form>
+            <TextField
+              label="Password"
+              type="password"
+              className="!mb-4"
+              fullWidth
+              placeholder="password"
+            />
+            <TextField
+              label="New password"
+              type="password"
+              className="!mb-4"
+              fullWidth
+              placeholder="new password"
+            />
+            <TextField
+              label="Repeated new password"
+              type="password"
+              className="!mb-4"
+              fullWidth
+              placeholder="password"
+            />
+            <div className="text-right">
+              <Button size="large" disabled variant="contained">
+                Update
+              </Button>
+            </div>
+          </form>
+          <h2 className="font-koulen text-3xl border-b-2 border-gray-200 border-solid mb-4">
+            Account
+          </h2>
+          <GoogleDeleteAccountButton />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Page
---
+++ app/(app-layout)/settings/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div>
+      <div className="w-52 h-10 bg-slate-100" />
+      <div className="w-16 h-6 mb-4 bg-slate-100" />
+    </div>
+  )
+}
