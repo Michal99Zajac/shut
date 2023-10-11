eb5d4f5542d132344cbd3d0e9c726e21756976ef
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-11 22:03:55 +0200

    fix(graphql): fix ssr uri link

--- client/graphql/server.tsx
+++ client/graphql/server.tsx
@@ -5,11 +5,13 @@ import {
 } from '@apollo/experimental-nextjs-app-support/ssr'
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc'
 
+import { config } from '@/config'
+
 export const { getClient } = registerApolloClient(() => {
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
     link: new HttpLink({
-      uri: `/graphql`,
+      uri: `${config.client.url}/graphql`,
     }),
   })
 })
