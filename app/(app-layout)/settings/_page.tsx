727427e0967f2002bd5456af9a5b878363e8bee6
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-13 20:01:14 +0200

    feat(app): add better settings page

--- app/(app-layout)/settings/_page.tsx
+++ app/(app-layout)/settings/_page.tsx
@@ -1,44 +1,39 @@
 'use client'
 
 import Breadcrumbs from '@mui/material/Breadcrumbs'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
 import Link from 'next/link'
 
-import GoogleDeleteAccountButton from '@/settings/components/GoogleDeleteAccountButton'
 import SecureForm from '@/settings/components/SecureForm'
+import DeleteAccountButton from '@/settings/components/DeleteAccountButton'
 
 const Page: Client.Page = () => {
   return (
     <>
       <h1 className="font-koulen text-4xl">Settings</h1>
-      <Breadcrumbs aria-label="breadcrumb" className="!mb-4">
+      <Breadcrumbs aria-label="breadcrumb" className="!mb-6">
         <Link href="/" className="font-koulen hover:underline">
           SHUT
         </Link>
         <p className="font-koulen">Settings</p>
       </Breadcrumbs>
-      <div className="flex justify-center mb-8">
-        <div className="w-[620px]">
-          <h2 className="font-koulen text-3xl border-b-2 border-gray-200 border-solid mb-6">
-            Profile
-          </h2>
-          <form>
-            <TextField label="Email" className="!mb-4" fullWidth placeholder="email@example.com" />
-            <div className="text-right">
-              <Button size="large" disabled variant="contained">
-                Update
-              </Button>
-            </div>
-          </form>
-          <h2 className="font-koulen text-3xl border-b-2 border-gray-200 border-solid mb-4">
-            Secure
-          </h2>
+      <div className="flex justify-center">
+        <div className="w-[640px] max-w-full">
+          <h2 className="font-koulen text-3xl mb-2">Secure</h2>
+          <p className="mb-4">
+            Set a new password. Changing your password will not log you out on other devices.
+          </p>
           <SecureForm />
-          <h2 className="font-koulen text-3xl border-b-2 border-gray-200 border-solid mb-4">
-            Account
-          </h2>
-          <GoogleDeleteAccountButton />
+          <hr className="my-8" />
+          <div className="p-4 rounded-md border-solid border-2 border-red-500 bg-red-100">
+            <h2 className="font-koulen text-3xl mb-6">Danger Zone</h2>
+            <h3 className="font-koulen text-xl mb-2">Delete Account</h3>
+            <p className="mb-4">
+              Permanently remove your Personal Account and all of its contents from the{' '}
+              <span className="font-koulen">SHUT</span>. This action is not reversible, so please
+              continue with caution.
+            </p>
+            <DeleteAccountButton />
+          </div>
         </div>
       </div>
     </>
