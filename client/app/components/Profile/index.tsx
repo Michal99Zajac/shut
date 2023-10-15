38731093bba464ea294d6b91460a0c22b5092e7f
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-15 13:39:00 +0200

    feat(Profile): set profile picture

--- client/app/components/Profile/index.tsx
+++ client/app/components/Profile/index.tsx
@@ -2,25 +2,24 @@
 
 import clsx from 'clsx'
 import Image from 'next/image'
-import { useMeSuspenseQuery } from '~/client/api/graphql/ssr'
+import { useMeSuspenseQuery } from '@/api/graphql/ssr'
+import { config } from '@/config'
 
 interface ProfileProps {
   className?: string
 }
+
+// TODO: clear cache with every logout
 
 export function Profile({ className }: ProfileProps) {
   const {
     data: { me },
   } = useMeSuspenseQuery()
 
+  const src = me.profileUrl || config.profile.defaultPicture
+
   return (
-    <Image
-      src="/media/profile/picture.png"
-      alt="avatar"
-      width={36}
-      height={36}
-      className={clsx('rounded', className)}
-    />
+    <Image src={src} alt="avatar" width={36} height={36} className={clsx('rounded', className)} />
   )
 }
 
