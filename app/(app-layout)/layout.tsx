450422308ef2d53da779d772dfb254d6db8b6c3b
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-15 18:04:12 +0200

    feat(app-layout): use ProfileMenu

--- app/(app-layout)/layout.tsx
+++ app/(app-layout)/layout.tsx
@@ -1,19 +1,29 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import useMedia from 'react-use/lib/useMedia'
+import dynamic from 'next/dynamic'
 
 import SignOutButton from '@/auth/components/SignOutButton'
 import Profile from '@/app/components/Profile'
+
+const ProfileMenu = dynamic(() => import('@/app/components/ProfileMenu'), {
+  loading: () => <div className="w-9 h-9 rounded bg-gray-100" />,
+})
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 export function Layout({ children }: LayoutProps) {
+  const isSmallScreen = useMedia('(max-width: 768px)')
+
   return (
     <>
       <header className="flex items-center justify-between p-4 mb-2">
         <Image src="/shut-logo.svg" alt="logo" width={32} height={42} />
-        <div className="flex gap-2 items-center">
+        <div className="hidden md:flex gap-2 items-center">
           <div className="inline-block">
             <Link
               className="font-koulen px-2 py-1 rounded-md hover:bg-primary hover:bg-opacity-5"
@@ -28,9 +38,10 @@ export function Layout({ children }: LayoutProps) {
               Settings
             </Link>
           </div>
-          <Profile className="mr-4" />
+          <Profile />
           <SignOutButton />
         </div>
+        {isSmallScreen && <ProfileMenu />}
       </header>
       <main className="p-4">{children}</main>
     </>
