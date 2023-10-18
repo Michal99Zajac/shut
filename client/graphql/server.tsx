7d2e1c261640935e4619f5818b18eeb67b51dba4
Michal99Zajac <michal.99.zajac@gmail.com>
2023-10-18 11:27:19 +0200

    feat(graphql): use the newest version of the server client

--- client/graphql/server.tsx
+++ client/graphql/server.tsx
@@ -1,15 +1,11 @@
-import { HttpLink } from '@apollo/client'
-import {
-  NextSSRInMemoryCache,
-  NextSSRApolloClient,
-} from '@apollo/experimental-nextjs-app-support/ssr'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc'
 
 import { config } from '@/config'
 
 export const { getClient } = registerApolloClient(() => {
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache(),
+  return new ApolloClient({
+    cache: new InMemoryCache(),
     link: new HttpLink({
       uri: `${config.client.url}/graphql`,
     }),
